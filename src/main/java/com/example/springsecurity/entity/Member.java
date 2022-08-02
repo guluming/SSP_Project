3c1049acaba49c87b2de6da9c61aa8113c9c9853
guluming <guluming@naver.com>
2022-08-02 21:59:07 +0900

    Feat: "추가 Member 클래스"
    - 기본적인 변수 추가 및 역할 구분을 위한 Enum 클래스 및 validator 클래스 추가

--- src/main/java/com/example/springsecurity/entity/Member.java
+++ src/main/java/com/example/springsecurity/entity/Member.java
@@ -1,4 +1,46 @@
 package com.example.springsecurity.entity;
 
+import com.example.springsecurity.dto.Request.MemberSignupRequest;
+import com.example.springsecurity.validator.MemberValidator;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import javax.persistence.*;
+
+@Entity
+@NoArgsConstructor
+@Getter
 public class Member {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(unique = true)
+    private String memberId;
+
+    @Column(nullable = false)
+    private String password;
+
+    @Column(unique = true)
+    private String nickname;
+
+    @Column(nullable = false)
+    @Enumerated(value = EnumType.STRING)
+    private MemberRoleEnum role;
+
+    public Member(MemberSignupRequest param, MemberRoleEnum role) {
+
+        MemberValidator.validatrMemberRegist(param);
+
+        this.memberId = param.getMemberId();
+        this.password = param.getPassword();
+        this.nickname = param.getPassword();
+        this.role = role;
+    }
+
+    public void encryptPassword(PasswordEncoder passwordEncoder) {
+        password = passwordEncoder.encode(password);
+    }
 }
---
+++ src/main/java/com/example/springsecurity/validator/MemberValidator.java
@@ -0,0 +1,9 @@
+package com.example.springsecurity.validator;
+
+import com.example.springsecurity.dto.Request.MemberSignupRequest;
+
+public class MemberValidator {
+    public static void validatrMemberRegist(MemberSignupRequest singUpData) {
+
+    }
+}
